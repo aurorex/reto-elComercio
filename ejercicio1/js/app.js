fd36bfd04d05e488312754e9c178ace41e5d7ea6
aurorex <selva_beat@hotmail.com>
2018-05-07 10:53:53 -0500

    agregando funcionalidad del ejercicio 1

---
+++ ejercicio1/js/app.js
@@ -0,0 +1,53 @@
+(function(){
+  var arrNumbers = [45, 75, 200, 256, 1, 27];
+
+  const buttonHigherNumber = document.getElementById('button-higher');
+  const buttonLessNumber = document.getElementById('button-less');
+  const buttonSecondNumber = document.getElementById('button-second-higher')
+  
+  // función para encontrar el número mayor
+  const higherNumber = () => {
+    var maxNumber = arrNumbers[0];
+
+    for(let i = 0;i < arrNumbers.length;i++) {
+      if(arrNumbers[i] > maxNumber) {
+        maxNumber = arrNumbers[i];
+      }
+    }
+    console.log('numero mayor:'+ maxNumber);
+  }
+
+  // función para encontra el número menor del arreglo
+  const lessNumber= () => {
+    var minNumber = arrNumbers[0];
+
+    for(let i = 0;i < arrNumbers.length;i++) {
+      if(minNumber > arrNumbers[i]) {
+        minNumber = arrNumbers[i];
+      }
+    }
+    console.log('numero menor: ' + minNumber);
+  };
+
+  // función para el encontrar el segundo número menor
+  const secondNumberHigher = () => {
+    var arrMaxNumber = arrNumbers[0];
+    var secondNumberHigher = arrNumbers[1];
+    for(let i = 0;i < arrNumbers.length;i++) {
+      if (arrNumbers[i] > arrMaxNumber) {
+        arrMaxNumber = arrNumbers[i];
+      }
+
+      if(arrNumbers[i] !== arrMaxNumber && arrNumbers[i] > secondNumberHigher ) {
+        secondNumberHigher =  arrNumbers[i];
+      }
+    }
+    console.log('segundo número mayor:'+ secondNumberHigher);
+  }
+
+  // eventos para los buttons de la interfaz grafica
+  buttonHigherNumber.addEventListener('click', higherNumber);
+  buttonLessNumber.addEventListener('click', lessNumber);
+  buttonSecondNumber.addEventListener('click', secondNumberHigher);
+
+})();
---
+++ ejercicio2/js/app2.js
@@ -0,0 +1,27 @@
+(function() {
+  const arrStrings = ['erik', 'leonardo', 'andres', 'gerardo', 'jan', 'renzo'];
+  
+  for(let i = 0;i < arrStrings.length; i++) {
+    // if(arrStrings[i].length > arrStrings[i+1].length) {
+    //   console.log('la cadena más larga:' + arrStrings[i])
+    //   let newString = arrStrings[i].substring(1, arrStrings[i].length )
+    //   let capitalLetterString = arrStrings[i].charAt(0);
+    //   console.log(capitalLetterString.toUpperCase() + newString );
+    // } else{
+    //   console.log('la cadena más larga:' + arrStrings[i+1]);
+    //   let newString = arrStrings[i+1].substring(1, arrStrings[i+1].length )
+    //   let capitalLetterString = arrStrings[i+1].charAt(0);
+    //   console.log(capitalLetterString.toUpperCase() + newString );
+    // }
+
+    // if(arrStrings[i] > arrStrings[i+1]) {
+    //   console.log('mayor1:'+arrStrings[i])
+
+    // } else {
+    //   console.log('mayor2:'+arrStrings[i+1])
+    // }
+  }
+
+
+
+})()
