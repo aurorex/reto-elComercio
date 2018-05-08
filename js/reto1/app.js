b9c4580897efbc88fa9a8dc1591ebf4e8305e5cf
aurorex <selva_beat@hotmail.com>
2018-05-08 07:37:17 -0500

    finalizando funcionalidad y estilos

--- js/reto1/app.js
+++ js/reto1/app.js
@@ -1,53 +1,47 @@
-(function(){
+(function() {
   var arrNumbers = [45, 75, 200, 256, 1, 27];
+  var resultElement = document.getElementById('result');
 
   const buttonHigherNumber = document.getElementById('button-higher');
   const buttonLessNumber = document.getElementById('button-less');
-  const buttonSecondNumber = document.getElementById('button-second-higher')
-  
+  const buttonSecondNumber = document.getElementById('button-second-higher');
   // función para encontrar el número mayor
   const higherNumber = () => {
     var maxNumber = arrNumbers[0];
-
-    for(let i = 0;i < arrNumbers.length;i++) {
-      if(arrNumbers[i] > maxNumber) {
+    for (let i = 0;i < arrNumbers.length;i++) {
+      if (arrNumbers[i] > maxNumber) {
         maxNumber = arrNumbers[i];
       }
     }
-    console.log('numero mayor:'+ maxNumber);
-  }
-
+    resultElement.innerHTML = maxNumber;
+  };
   // función para encontra el número menor del arreglo
-  const lessNumber= () => {
+  const lessNumber = () => {
     var minNumber = arrNumbers[0];
-
-    for(let i = 0;i < arrNumbers.length;i++) {
-      if(minNumber > arrNumbers[i]) {
+    for (let i = 0;i < arrNumbers.length;i++) {
+      if (minNumber > arrNumbers[i]) {
         minNumber = arrNumbers[i];
       }
     }
-    console.log('numero menor: ' + minNumber);
+    resultElement.innerHTML = minNumber;
   };
-
   // función para el encontrar el segundo número menor
   const secondNumberHigher = () => {
     var arrMaxNumber = arrNumbers[0];
     var secondNumberHigher = arrNumbers[1];
-    for(let i = 0;i < arrNumbers.length;i++) {
+
+    for (let i = 0;i < arrNumbers.length;i++) {
       if (arrNumbers[i] > arrMaxNumber) {
         arrMaxNumber = arrNumbers[i];
       }
-
-      if(arrNumbers[i] !== arrMaxNumber && arrNumbers[i] > secondNumberHigher ) {
-        secondNumberHigher =  arrNumbers[i];
+      if (arrNumbers[i] !== arrMaxNumber && arrNumbers[i] > secondNumberHigher) {
+        secondNumberHigher = arrNumbers[i];
       }
     }
-    console.log('segundo número mayor:'+ secondNumberHigher);
-  }
-
+    resultElement.innerHTML = secondNumberHigher;
+  };
   // eventos para los buttons de la interfaz grafica
   buttonHigherNumber.addEventListener('click', higherNumber);
   buttonLessNumber.addEventListener('click', lessNumber);
   buttonSecondNumber.addEventListener('click', secondNumberHigher);
-
 })();
--- js/reto2/app2.js
+++ js/reto2/app2.js
@@ -1,49 +1,44 @@
 (function() {
-  const arrStrings = ['erik', 'leonardo', 'andresito', 'gerardo', 'jan', 'renzo'];
+  var arrStrings = ['erik', 'leonardo', 'andres', 'gerardo', 'jan', 'renzo'];
+  var result = document.getElementById('result2');
 
   const buttonCapitalLetter = document.getElementById('button-capital-letter');
   const buttonAscendingOrder = document.getElementById('button-ascending-order');
-  
+  // función para encontrar la cadena más larga y que empiece con inicial mayúscula;
   const firstCapitalLetter = () => {
     var longWord = arrStrings[0];
-    var newWord = []; 
-    var capitalLetter = '';
-    
-    for(let i = 0;i < arrStrings.length; i++) {
-      if(arrStrings[i].length > longWord.length) {
+    var auxString = '';
+    for (let i = 1;i < arrStrings.length; i++) {
+      if (arrStrings[i].length > longWord.length) {
         longWord = arrStrings[i];
-        capitalLetter = longWord[0].toUpperCase();
-        longWord[1] === capitalLetter;
       } 
     }
-    console.log(capitalLetter);
-    console.log('la cadena más larga:' +capitalLetter+longWord);
-  }
 
-  const ascendingOrder = () => {
-    var lastLetterAlfabet = arrStrings[0];
-    var firstLetterAlfabet = arrStrings[0];
-
-    for(let i = 0;i < arrStrings.length; i++) {
-      if (arrStrings[i] >lastLetterAlfabet ){
-        lastLetterAlfabet = arrStrings[i];
-        console.log('ultima palabra del arreglo:'+ lastLetterAlfabet);
-      }
-      if (arrStrings[i] < firstLetterAlfabet){
-        firstLetterAlfabet = arrStrings[i];
-        console.log('primera palabra del arreglo:'+ firstLetterAlfabet);
-
-      }
-      // if(arrStrings[i] > lastLetterAlfabet ) {
-      //   lastLetterAlfabet = arrStrings[i];
-      // }
+    for (let j = 1;j < longWord.length; j++) {
+      auxString = auxString + longWord[j];
     }
 
-    
-    
-  }
-
+    var firstCapitalLetter = longWord[0].toUpperCase();
+    auxString = firstCapitalLetter + auxString;
+    console.log('la cadena más larga:' + auxString);
+    result.innerHTML = auxString;
+  };
+  // función para ordenar los elementos del arreglo en forma ascendente(a-z) y mostrar la nueva lista
+  const ascendingOrder = () => {
+    var auxString = '';
+    for (let i = 0;i < arrStrings.length; i++) {
+      for (let j = i;j < arrStrings.length;j++) {
+        if (arrStrings[i] > arrStrings[j]) {
+          auxString = arrStrings[i];
+          arrStrings[i] = arrStrings[j];
+          arrStrings[j] = auxString;
+        }
+      }
+    }
+    console.log(arrStrings);
+    result.innerHTML = arrStrings;
+  };
+  // eventos para los buttons
   buttonCapitalLetter.addEventListener('click', firstCapitalLetter);
   buttonAscendingOrder.addEventListener('click', ascendingOrder);
-
-})()
+})();
